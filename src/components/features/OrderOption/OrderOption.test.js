1e30ec3f5b60fc51649a2e097d7f05d91a64f11b
naz <naz@o2.pl>
2021-04-20 12:07:13 +0200

    Add more tests for OrderOption and subtest schema

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -3,9 +3,54 @@ import { shallow } from 'enzyme';
 import OrderOption from './OrderOption';
 
 describe('Component OrderOption', () => {
+  const expectedName = 'name';
+  const expectedType = 'type';
+
   it('should render without crashing', () => {
-    const component = shallow(<OrderOption name="name" type="type" />);
+    const component = shallow(
+      <OrderOption name={expectedName} type={expectedType} />
+    );
     expect(component).toBeTruthy();
-    //console.log(component.debug());
+    console.log(component.debug());
+  });
+
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+
+  it('should render correct title', () => {
+    const component = shallow(
+      <OrderOption name={expectedName} type={expectedType} />
+    );
+    expect(component.find('.title').text()).toEqual(expectedName);
   });
 });
+
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+for (let type in optionTypes) {
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */
+
+    /* common tests */
+    it('passes dummy test', () => {
+      expect(1).toBe(1);
+    });
+
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        /* tests for dropdown */
+        break;
+      }
+    }
+  });
+}
