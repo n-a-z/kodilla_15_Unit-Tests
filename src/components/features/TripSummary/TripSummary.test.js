2bd9f97e819f919070be7d567cf0fd5bac6e3634
naz <naz@o2.pl>
2021-04-20 09:51:18 +0200

    Add TripSummary tag tests

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -48,9 +48,25 @@ describe('Component TripSummary', () => {
 
   it('should throw an error without required props', () => {
     expect(() => <TripSummary />).toThrow();
-    expect(() => <TripSummary id='id' />).toThrow();
-    expect(() => <TripSummary id='id' name='name' />).toThrow();
-    expect(() => <TripSummary id='id' name='name' cost='cost' />).toThrow();
-    expect(() => <TripSummary id='id' name='name' cost='cost' days={1} />).toThrow();
+    expect(() => <TripSummary id="id" />).toThrow();
+    expect(() => <TripSummary id="id" name="name" />).toThrow();
+    expect(() => <TripSummary id="id" name="name" cost="cost" />).toThrow();
+    expect(() => (
+      <TripSummary id="id" name="name" cost="cost" days={1} />
+    )).toThrow();
+  });
+
+  it('should render array with elements in right order', () => {
+    const expectedTags = ['tag1', 'tag2', 'tag3'];
+    const component = shallow(<TripSummary tags={expectedTags} />);
+
+    expect(component.find('.tag').at(0).text()).toEqual(expectedTags[0]);
+    expect(component.find('.tag').at(1).text()).toEqual(expectedTags[1]);
+    expect(component.find('.tag').at(2).text()).toEqual(expectedTags[2]);
+  });
+
+  it('should not render div tags if props tags is falsy', () => {
+    const component = shallow(<TripSummary />);
+    expect(component.hasClass('tags')).toBeFalsy();
   });
 });
